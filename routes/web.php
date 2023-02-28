25f9fa560669cc2f1a6c42e505e41e727c35dc77
DUONG\admin <hoangnghiaduong0@gmail.com>
2023-02-28 10:02:31 +0700

    create login page

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/login-page', function(){
+    return view('login-page');
 });
